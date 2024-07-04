94dccafb0d444ffc714033f2794bd9ea134ce4f9
easyomi-code <easyomi.code@gmail.com>
2024-07-04 20:31:56 +0900

    Add Operator

--- lectures/3_javascript/operator.js
+++ lectures/3_javascript/operator.js
@@ -7,4 +7,23 @@ console.log('10' + 0 + 4); // 1004 출력
 console.log(3 + 6 + '9'); // 99 출력
 console.log(typeof (3 + 6 + '9')); // string 타입
 
+console.log('===========================');
+
 // 산술 연산자
+let x = 10;
+let y = 20;
+console.log(x + y);
+console.log(x - y);
+console.log(x / y);
+console.log(x * y);
+console.log(10 % 3);
+
+// 실습) 사용자로부터 숫자를 입력받아 홀수, 짝수 판별하기
+let number = prompt('숫자를 입력해주세요!');
+if (number % 2 == 1) {
+  alert(`숫자 ${number}은 홀수입니다.`);
+} else {
+  alert(`숫자 ${number}은 짝수입니다.`);
+}
+
+// 증감 연산자
