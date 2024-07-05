6c9418ec9c5ebc825e9ab2f3327a43291d7722a0
easyomi-code <easyomi.code@gmail.com>
2024-07-05 23:11:50 +0900

    Add Operator-2

---
+++ lectures/3_javascript/operator-2.js
@@ -0,0 +1,8 @@
+// 논리 연산자(AND, OR, NOT)
+// JavaScript는 다른 언어와 다르게 피연산자가 불리언(boolean) 타입이 아닐때도 논리 연산이 가능하다.
+// 값이 있는 것을 true로 간주하기 때문
+
+// AND(&&) 연산 - 피연산자가 모두 true일 경우에 ture 반환, 나머지 false
+let x = true;
+let y = true;
+console.log(x && y);
